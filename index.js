710f98d9bbb348752ec8e45a78afac2623410b4a
mnavoy4 <mnavoy4@gmail.com>
2020-08-07 13:38:23 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+function map(array, fn){
+  let newArray = [];
+  for (let i = 0; i < array.length; i++){
+    let element = array[i];
+    newArray.push(fn(element))
+  }
+  return newArray
+}
+
+function reduce(array, fn, startingPoint){
+  let r = (!!startingPoint) ? startingPoint : array[0];
+  let i = (!!startingPoint) ? 0 : 1;
+  for (; i < array.length; i++){
+    let element = array[i];
+    r = fn(element, r);
+  }
+  return r;
+}
